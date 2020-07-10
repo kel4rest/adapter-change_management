88d7745b4ca5d0ce5933a6fea55e40f55c84ed4c
kel4rest <kelly.forrest@centurylink.com>
2020-07-10 18:43:19 +0000

    Lab 2

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ function post(serviceNowTable, callback) {
       pass: options.password,
     },
     baseUrl: options.url,
-    uri: `/api/now/table/${serviceNowTable}`
+    uri: `/api/now/table/${serviceNowTable}`,
   };
 
   // Send Request to ServiceNow.
